980db2376fc1f62bc5a3af35f1808562ae0606fb
dernounimk <mohameddernouni13@gmail.com>
2025-11-22 14:26:06 +0100

    وصف التغييرات التي قمت بها

--- backend/routes/order.route.js
+++ backend/routes/order.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createOrder, deleteOrder, getAllOrders, toggleConfirmOrders, followOrder, editOrder } from "../controllers/order.controller.js";
+import { createOrder, deleteOrder, getAllOrders, toggleConfirmOrders, editOrder } from "../controllers/order.controller.js";
 import { adminRoute, protectRoute } from "../middleware/auth.middleware.js";
 const router = express.Router();
 
@@ -8,6 +8,5 @@ router.put("/:orderId", protectRoute, adminRoute, editOrder);
 router.get("/", protectRoute, adminRoute, getAllOrders);
 router.delete("/:id", protectRoute, adminRoute, deleteOrder);
 router.patch("/toggle-confirm", protectRoute, adminRoute, toggleConfirmOrders);
-router.post("/follow-order", followOrder);
 
 export default router;
